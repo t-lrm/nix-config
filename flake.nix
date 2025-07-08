8f33e54016fa543435bc055bdddf7d2bedb35d4c
Timothée Le Roux Maertens <timothee.le-roux-maertens@epita.fr>
2025-07-08 10:40:32 +0200

    Update: moving files

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My NixOS laptop configuration";
+  description = "My NixOS configurations";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
@@ -7,7 +7,11 @@
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ /home/baunky/Documents/Github/nix-config ];
+        modules = [ ./hosts/laptop/configuration.nix ];
+      };
+      server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/server/configuration.nix ];
       };
     };
   };
