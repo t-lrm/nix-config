eae2c0a3e2ddd2391ddf279f36e8bf942967b0d8
Timothée Le Roux Maertens <timothee.le-roux-maertens@epita.fr>
2025-07-08 11:10:32 +0200

    Fix: audio driver issues

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -59,7 +59,8 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # services.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
