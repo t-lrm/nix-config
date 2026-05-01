92eae6d5fc77f93e673ab6bd8c0ef1933719602d
Timothée Le Roux Maertens <timothee.le-roux-maertens@epita.fr>
2026-05-01 18:26:56 +0700

    core: rework flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "My NixOS, nix-darwin, and Home Manager configurations";
 
-  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
@@ -21,83 +20,99 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    nix-darwin,
-    home-manager,
-    nix-index-database,
-    ...
-  }: let
-    mkArgs = {
-      host,
-      username,
-      system,
-    }: let
-      vars = import ./hosts/variables.nix;
-    in {
-      inherit inputs host username system vars;
-      myPkgs = self.packages.${system} or {};
-    };
-
-    mkHomeManagerModule = {
-      host,
-      username,
-      system,
-    }: {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.backupFileExtension = "home-manager-backup";
-
-      home-manager.extraSpecialArgs = mkArgs {inherit host username system;};
-
-      home-manager.users.${username} = import ./hosts/${host}/home.nix;
-    };
-
-    mkNixosHost = {
-      host,
-      username,
-      system,
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      nix-index-database,
+      ...
     }:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = mkArgs {inherit host username system;};
+    let
+      mkArgs =
+        {
+          host,
+          username,
+          system,
+        }:
+        let
+          vars = import ./hosts/variables.nix;
+        in
+        {
+          inherit
+            inputs
+            host
+            username
+            system
+            vars
+            ;
+          myPkgs = self.packages.${system} or { };
+        };
 
-        modules = [
-          ./hosts/${host}/configuration.nix
-          nix-index-database.nixosModules.default
+      mkHomeManagerModule =
+        {
+          host,
+          username,
+          system,
+        }:
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "home-manager-backup";
 
-          home-manager.nixosModules.home-manager
-          (mkHomeManagerModule {inherit host username system;})
-        ];
+          home-manager.extraSpecialArgs = mkArgs { inherit host username system; };
+
+          home-manager.users.${username} = import ./hosts/${host}/home.nix;
+        };
+
+      mkNixosHost =
+        {
+          host,
+          username,
+          system,
+        }:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = mkArgs { inherit host username system; };
+
+          modules = [
+            ./hosts/${host}/configuration.nix
+            nix-index-database.nixosModules.default
+
+            home-manager.nixosModules.home-manager
+            (mkHomeManagerModule { inherit host username system; })
+          ];
+        };
+
+      mkDarwinHost =
+        {
+          host,
+          username,
+          system,
+        }:
+        nix-darwin.lib.darwinSystem {
+          specialArgs = mkArgs { inherit host username system; };
+
+          modules = [
+            ./hosts/${host}/configuration.nix
+            nix-index-database.darwinModules.nix-index
+
+            home-manager.darwinModules.home-manager
+            (mkHomeManagerModule { inherit host username system; })
+          ];
+        };
+    in
+    {
+      nixosConfigurations.thinkpad = mkNixosHost {
+        host = "thinkpad";
+        username = "nixos";
+        system = "x86_64-linux";
       };
 
-    mkDarwinHost = {
-      host,
-      username,
-      system,
-    }:
-      nix-darwin.lib.darwinSystem {
-        specialArgs = mkArgs {inherit host username system;};
-
-        modules = [
-          ./hosts/${host}/configuration.nix
-          nix-index-database.darwinModules.nix-index
-
-          home-manager.darwinModules.home-manager
-          (mkHomeManagerModule {inherit host username system;})
-        ];
+      darwinConfigurations.macbook = mkDarwinHost {
+        host = "macbook";
+        username = "baunky";
+        system = "aarch64-darwin";
       };
-  in {
-    nixosConfigurations.thinkpad = mkNixosHost {
-      host = "thinkpad";
-      username = "nixos";
-      system = "x86_64-linux";
     };
-
-    darwinConfigurations.macbook = mkDarwinHost {
-      host = "macbook";
-      username = "baunky";
-      system = "aarch64-darwin";
-    };
-  };
 }
