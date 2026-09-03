c70ff4f1a0b000cce601ab220d57143ef2b69d6c
Timothée Le Roux Maertens <timothee.le-roux-maertens@epita.fr>
2026-09-03 21:53:44 +0200

    fix: afs.sh

--- hosts/epita/afs.sh
+++ hosts/epita/afs.sh
@@ -2,4 +2,11 @@
 # kinit -f timothee.le-roux-maertens@CRI.EPITA.FR
 
 mkdir -p "$HOME/afs"
-sshfs -o reconnect timothee.le-roux-maertens@ssh.cri.epita.fr:/afs/cri.epita.fr/user/t/ti/timothee.le-roux-maertens/u/ afs
+
+sshfs \
+  -o reconnect \
+  -o PubkeyAuthentication=no \
+  -o GSSAPIAuthentication=yes \
+  -o GSSAPIDelegateCredentials=yes \
+  timothee.le-roux-maertens@ssh.cri.epita.fr:/afs/cri.epita.fr/user/t/ti/timothee.le-roux-maertens/u/ \
+  ~/afs
